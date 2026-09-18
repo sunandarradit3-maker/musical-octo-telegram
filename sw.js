2ec4bf9de26f32b88a9f175935f9c03ac2d316e6
ditzstoreofficial <sunandarradit3@gmail.com>
2026-09-18 19:22:51 +0700

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='temuinhp-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
+});
